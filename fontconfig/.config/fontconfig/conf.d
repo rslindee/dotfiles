0a1f661cff257f8029a879c4301e1489013f4d3b
Richard Slindee <katzinator@gmail.com>
2017-11-19 13:30:41 -0800

    fontconfig: Add 'Terminus' to 'xos4 Terminus' alias

---
+++ fontconfig/.config/fontconfig/conf.d
@@ -0,0 +1,10 @@
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+<!-- Fix the name of Terminus font from 'Terminus' to 'xos4 Terminus' -->
+  <alias>
+    <family>Terminus</family>
+    <prefer><family>xos4 Terminus</family></prefer>
+    <default><family>fixed</family></default>
+  </alias>
+</fontconfig>
