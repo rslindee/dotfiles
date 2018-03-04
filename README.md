ee3d7ed1600d7b28cdf9632fa7c54f7a7377f943
Richard Slindee <katzinator@gmail.com>
2018-03-03 17:11:47 -0800

    Massive cleanup, reorg, and add install script

---
+++ README.md
@@ -0,0 +1,12 @@
+## Overview
+These are all my dotfiles to be used with GNU Stow
+
+## Install script
+Note that this install script won't work for you, as it is specifically tailored for my setup. With that said, feel free to examine, modify, and repurpose it for your needs.
+
+### Prerequisites
+* ssh key has been copied to ~/.ssh
+* git is installed
+
+### Installation
+`git clone git@github.com:rslindee/dotfiles.git $HOME/dotfiles && $HOME/dotfiles/install.sh`
---
+++ install.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# Fail right away
+set -e
+
+# TODO: Confirm how/if these need to be installed on new system
+# xdg-utils xorg-xmodmap
+
+ALL_PACKAGES="atool \
+    cgdb \
+    chromium \
+    clang \
+    ctags \
+    fontconfig \
+    i3-wm \
+    mpd \
+    mpv \
+    ncmpcpp \
+    neomutt \
+    newsboat \
+    python \
+    qutebrowser \
+    ranger \
+    renameutils \
+    stow \
+    sxiv \
+    tmux \
+    vim \
+    xclip \
+    zathura \
+    zsh"
+
+PACKAGES_FEDORA="st \
+    terminus-fonts-console"
+
+FEDORA_COPR_REPOS="fszymanski/newsboat \
+    flatcap/neomutt"
+
+PACKAGES_ARCH="terminus-font"
+PACKAGES_AUR="st"
+
+STOW_LIST="cgdb \
+    clang \
+    ctags \
+    fontconfig \
+    git \
+    i3 \
+    mimeapps \
+    mpd \
+    mutt \
+    ncmpcpp \
+    qutebrowser \
+    ranger \
+    ssh \
+    tmux \
+    vim \
+    xbindkeys \
+    zsh"
+
+STOW_LIST_PRIVATE="newsboat"
+
+# Get OS version
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+fi
+
+if [ "$OS" = "Fedora" ]; then
+    echo "Fedora system detected..."
+    PKG_MANAGER_INSTALL="sudo dnf install"
+    # Setup RPM Fusion
+    sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    # Install copr plugins package first
+    $PACKAGE_MANAGER_INSTALL dnf-plugins-core
+    # TODO how do we handle multiple COPR repos (e.g. single command)?
+    sudo dnf copr enable $FEDORA_COPR_REPOS
+    # Create package list
+    ALL_PACKAGES="$ALL_PACKAGES $PACKAGES_FEDORA $PACKAGES_AUR"
+elif [ "$OS" = "Arch Linux" ]; then
+    echo "Arch system detected..."
+    # Install trizen
+    git clone https://aur.archlinux.org/trizen.git $HOME/trizen
+    cd $HOME/trizen
+    makepkg -si
+    PKG_MANAGER_INSTALL="trizen -S"
+    # Create package list
+    ALL_PACKAGES="$ALL_PACKAGES $PACKAGES_ARCH $PACKAGES_AUR"
+fi
+
+# Install packages
+$PKG_MANAGER_INSTALL $ALL_PACKAGES
+
+# Stow dotfiles
+cd $HOME/dotfiles
+stow $STOW_LIST
+
+# Clone private repos
+git clone git@gitlab.com:rslindee/wiki.git $HOME/wiki
+git clone git@gitlab.com:rslindee/dotfiles-private.git $HOME/dotfiles-private
+
+# Stow private dotfiles
+cd $HOME/dotfiles-private
+stow $STOW_LIST_PRIVATE
