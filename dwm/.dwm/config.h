43e52b1658e90f71e76305024b7863dd2a133669
Richard Slindee <richard@slindee.net>
2020-05-17 12:04:35 -0700

    dwm: format config

--- dwm/.dwm/config.h
+++ dwm/.dwm/config.h
@@ -68,58 +68,58 @@ static const char *passmenu_cmd[] = { "passmenu", "-i", "-fn", dmenufont, "-nb",
 static const char *term_cmd[]  = { "st", "-f", "Terminus:size=12", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_a,      zoom,           {0} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_c,      spawn,          SHCMD("mpc toggle") },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_g,      spawn,          {.v = passmenu_cmd } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY,							          XK_m,      spawn,				   SHCMD("amixer set Master toggle") },
-	{ MODKEY|ShiftMask,             XK_n,      spawn,          SHCMD("mpc next") },
-	{ MODKEY,                       XK_o,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("mpc prev") },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY,                       XK_s,      spawn,          SHCMD("import -silent ~/screenshot-$(date \"+%Y-%m-%d-%H:%M:%S\").png") },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("import -silent -window root ~/screenshot-$(date \"+%Y-%m-%d-%H:%M:%S\").png") },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_t,      spawn,          SHCMD("add-torrents.sh") },
-	{ MODKEY,                       XK_v,      spawn,          SHCMD("echo 'cycle pause' | socat - /tmp/mpv-socket") },
-	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_grave,  spawn,          SHCMD("xrandr_util.sh -s") },
-	{ MODKEY|ShiftMask,             XK_grave,  spawn,          SHCMD("xrandr_util.sh -p") },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ 0,                            XF86MonBrightnessUp,       spawn,       SHCMD("light -A 10") },
-	{ 0,                            XF86MonBrightnessDown,     spawn,       SHCMD("light -U 10") },
-  { 0,							              XF86AudioMute,             spawn,				SHCMD("amixer set Master toggle") },
-  { 0,							              XF86AudioRaiseVolume,      spawn,				SHCMD("amixer set Master 5%+") },
-  { 0,							              XF86AudioLowerVolume,      spawn,				SHCMD("amixer set Master 5%-") },
-  { 0,							              XF86AudioMicMute,				   spawn,				SHCMD("amixer set Capture toggle") },
-	{ MODKEY,                       XK_BackSpace,              spawn,         SHCMD("slock") },
-	{ MODKEY,                       XK_Return,                 spawn,         {.v = term_cmd } },
+	/* modifier         key                    function        argument */
+	{ MODKEY,           XK_a,                  zoom,           {0} },
+	{ MODKEY,           XK_b,                  togglebar,      {0} },
+	{ MODKEY,           XK_c,                  spawn,          SHCMD("mpc toggle") },
+	{ MODKEY,           XK_d,                  incnmaster,     {.i = -1 } },
+	{ MODKEY,           XK_f,                  setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,           XK_g,                  spawn,          {.v = passmenu_cmd } },
+	{ MODKEY,           XK_h,                  setmfact,       {.f = -0.05} },
+	{ MODKEY,           XK_i,                  incnmaster,     {.i = +1 } },
+	{ MODKEY,           XK_j,                  focusstack,     {.i = +1 } },
+	{ MODKEY,           XK_k,                  focusstack,     {.i = -1 } },
+	{ MODKEY,           XK_l,                  setmfact,       {.f = +0.05} },
+  { MODKEY,           XK_m,                  spawn,          SHCMD("amixer set Master toggle") },
+	{ MODKEY|ShiftMask, XK_n,                  spawn,          SHCMD("mpc next") },
+	{ MODKEY,           XK_o,                  spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask, XK_p,                  spawn,          SHCMD("mpc prev") },
+	{ MODKEY,           XK_q,                  killclient,     {0} },
+	{ MODKEY|ShiftMask, XK_q,                  quit,           {0} },
+	{ MODKEY,           XK_s,                  spawn,          SHCMD("import -silent ~/screenshot-$(date \"+%Y-%m-%d-%H:%M:%S\").png") },
+	{ MODKEY|ShiftMask, XK_s,                  spawn,          SHCMD("import -silent -window root ~/screenshot-$(date \"+%Y-%m-%d-%H:%M:%S\").png") },
+	{ MODKEY,           XK_t,                  setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask, XK_t,                  spawn,          SHCMD("add-torrents.sh") },
+	{ MODKEY,           XK_v,                  spawn,          SHCMD("echo 'cycle pause' | socat - /tmp/mpv-socket") },
+	{ MODKEY,           XK_z,                  setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,           XK_space,              setlayout,      {0} },
+	{ MODKEY|ShiftMask, XK_space,              togglefloating, {0} },
+	{ MODKEY,           XK_Tab,                view,           {0} },
+	{ MODKEY,           XK_0,                  view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask, XK_0,                  tag,            {.ui = ~0 } },
+	{ MODKEY,           XK_comma,              focusmon,       {.i = -1 } },
+	{ MODKEY,           XK_period,             focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask, XK_comma,              tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask, XK_period,             tagmon,         {.i = +1 } },
+	{ MODKEY,           XK_grave,              spawn,          SHCMD("xrandr_util.sh -s") },
+	{ MODKEY|ShiftMask, XK_grave,              spawn,          SHCMD("xrandr_util.sh -p") },
+	{ 0,                XF86MonBrightnessUp,   spawn,          SHCMD("light -A 10") },
+	{ 0,                XF86MonBrightnessDown, spawn,          SHCMD("light -U 10") },
+  { 0,                XF86AudioMute,         spawn,          SHCMD("amixer set Master toggle") },
+  { 0,                XF86AudioRaiseVolume,  spawn,          SHCMD("amixer set Master 5%+") },
+  { 0,                XF86AudioLowerVolume,  spawn,          SHCMD("amixer set Master 5%-") },
+  { 0,                XF86AudioMicMute,      spawn,          SHCMD("amixer set Capture toggle") },
+	{ MODKEY,           XK_BackSpace,          spawn,          SHCMD("slock") },
+	{ MODKEY,           XK_Return,             spawn,          {.v = term_cmd } },
+	TAGKEYS(            XK_1,                  0)
+	TAGKEYS(            XK_2,                  1)
+	TAGKEYS(            XK_3,                  2)
+	TAGKEYS(            XK_4,                  3)
+	TAGKEYS(            XK_5,                  4)
+	TAGKEYS(            XK_6,                  5)
+	TAGKEYS(            XK_7,                  6)
+	TAGKEYS(            XK_8,                  7)
+	TAGKEYS(            XK_9,                  8)
 };
 
 /* button definitions */
