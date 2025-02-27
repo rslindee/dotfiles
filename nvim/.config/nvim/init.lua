0a55ecee392cbfcb7c33bd8e37afc78e62265394
Richard Slindee <rslindee@ford.com>
2025-02-27 15:10:41 -0800

    nvim: tweak copilot chat width

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -212,6 +212,19 @@ require("lazy").setup({
     opts = {
       debug = true, -- Enable debugging
       -- See Configuration section for rest
+      window = {
+        layout = 'vertical', -- 'vertical', 'horizontal', 'float', 'replace'
+        width = 0.3, -- fractional width of parent, or absolute width in columns when > 1
+        height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
+        -- Options below only apply to floating windows
+        relative = 'editor', -- 'editor', 'win', 'cursor', 'mouse'
+        border = 'single', -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+        row = nil, -- row position of the window, default is centered
+        col = nil, -- column position of the window, default is centered
+        title = 'Copilot Chat', -- title of chat window
+        footer = nil, -- footer of chat window
+        zindex = 1, -- determines if window is on top or below other floating windows
+      },
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
