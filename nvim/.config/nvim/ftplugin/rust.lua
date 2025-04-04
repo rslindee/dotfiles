78597802b52aedc8c4ec286c729571df6f4c60bc
Richard Slindee <rslindee@ford.com>
2025-04-04 12:29:12 -0700

    nvim: updates rustfmt

--- nvim/.config/nvim/ftplugin/rust.lua
+++ nvim/.config/nvim/ftplugin/rust.lua
@@ -1,3 +1,5 @@
+vim.g.rustfmt_command = 'rustfmt --edition 2021'
+
 vim.keymap.set('n', '<leader>i', ":RustFmt<cr>", { noremap = true, silent = true, desc = "Run rustfmt on current file" })
 vim.keymap.set('n', '<leader>mm', ':silent !cargo run<cr>', { noremap = true, silent = true, desc = "Cargo run"})
 vim.cmd('compiler cargo')
