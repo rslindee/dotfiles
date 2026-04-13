94a211b073467e2c71602daa72299eddc4d3bd0c
Richard Slindee <rslindee@ford.com>
2026-04-13 14:01:37 -0700

    WIP, using new treesitter init

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,8 +44,83 @@ require("lazy").setup({
 		end,
 	},
 	-- nvim treesitter
-	-- TODO: update "master" to "main" for nvim 0.12
-	{ "nvim-treesitter/nvim-treesitter", branch = "master", build = ":TSUpdate" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter")
+			ts.setup({
+				-- keep highlighting and textobject behaviour from previous config
+				highlight = { enable = true, additional_vim_regex_highlighting = false },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]a"] = "@argument.outer",
+							["]m"] = "@method.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[a"] = "@argument.outer",
+							["[m"] = "@method.outer",
+						},
+					},
+				},
+				auto_install = true,
+				sync_install = false,
+			})
+			-- Install parsers explicitly (keeps behaviour similar to ensure_installed)
+			ts.install({
+				"c",
+				"lua",
+				"vim",
+				"query",
+				"markdown",
+				"rust",
+				"java",
+				"elixir",
+				"heex",
+				"javascript",
+				"typescript",
+				"html",
+				"yaml",
+			})
+			-- Ensure treesitter features are started per-filetype and enable nvim-treesitter indentation
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"c",
+					"lua",
+					"vim",
+					"query",
+					"markdown",
+					"rust",
+					"java",
+					"elixir",
+					"heex",
+					"javascript",
+					"typescript",
+					"html",
+					"yaml",
+				},
+				callback = function()
+					vim.treesitter.start()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
 	-- treesitter textobjects
 	{ "nvim-treesitter/nvim-treesitter-textobjects", dependencies = { "nvim-treesitter/nvim-treesitter" } },
 	-- gruvbox theme
@@ -835,57 +910,57 @@ vim.keymap.set("n", "<leader>td", ":Gitsigns toggle_deleted<CR>")
 -- Gitsigns Text object
 vim.keymap.set({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 
-require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all"
-	ensure_installed = { "c", "lua", "vim", "query", "markdown", "rust" },
-
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
-
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
-
-	highlight = {
-		enable = true,
-
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
-
-	-- requires textobjects plugin
-	textobjects = {
-		select = {
-			enable = true,
-			-- Automatically jump forward to textobj, similar to targets.vim
-			lookahead = true,
-
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-			},
-		},
-		move = {
-			enable = true,
-			set_jumps = true,
-			goto_next_start = {
-				["]f"] = "@function.outer",
-				["]a"] = "@argument.outer",
-				["]m"] = "@method.outer",
-			},
-			goto_previous_start = {
-				["[f"] = "@function.outer",
-				["[a"] = "@argument.outer",
-				["[m"] = "@method.outer",
-			},
-		},
-	},
-})
-
+-- require("nvim-treesitter.configs").setup({
+-- 	-- A list of parser names, or "all"
+-- 	ensure_installed = { "c", "lua", "vim", "query", "markdown", "rust" },
+--
+-- 	-- Install parsers synchronously (only applied to `ensure_installed`)
+-- 	sync_install = false,
+--
+-- 	-- Automatically install missing parsers when entering buffer
+-- 	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+-- 	auto_install = true,
+--
+-- 	highlight = {
+-- 		enable = true,
+--
+-- 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+-- 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+-- 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+-- 		-- Instead of true it can also be a list of languages
+-- 		additional_vim_regex_highlighting = false,
+-- 	},
+--
+-- 	-- requires textobjects plugin
+-- 	textobjects = {
+-- 		select = {
+-- 			enable = true,
+-- 			-- Automatically jump forward to textobj, similar to targets.vim
+-- 			lookahead = true,
+--
+-- 			keymaps = {
+-- 				-- You can use the capture groups defined in textobjects.scm
+-- 				["af"] = "@function.outer",
+-- 				["if"] = "@function.inner",
+-- 			},
+-- 		},
+-- 		move = {
+-- 			enable = true,
+-- 			set_jumps = true,
+-- 			goto_next_start = {
+-- 				["]f"] = "@function.outer",
+-- 				["]a"] = "@argument.outer",
+-- 				["]m"] = "@method.outer",
+-- 			},
+-- 			goto_previous_start = {
+-- 				["[f"] = "@function.outer",
+-- 				["[a"] = "@argument.outer",
+-- 				["[m"] = "@method.outer",
+-- 			},
+-- 		},
+-- 	},
+-- })
+--
 -- reload vimrc
 vim.keymap.set("n", "<leader>vv", ":source $MYVIMRC<CR>")
 
