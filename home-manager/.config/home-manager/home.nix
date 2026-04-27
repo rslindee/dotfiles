8bdcb1c814b8475509e6b1554fc745e4b395d666
Richard Slindee <rslindee@ford.com>
2026-04-27 10:37:03 -0700

    home-manager: initial checkin

---
+++ home-manager/.config/home-manager/home.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ...}: 
+{
+  home.username = "rslindee";
+  home.homeDirectory = "/home/rslindee";
+  home.stateVersion = "25.11";
+
+  targets.genericLinux.enable = true;
+
+  home.packages = with pkgs; [
+    curl
+    direnv
+    fzf
+    jq
+    protols
+    ripgrep
+    tmux
+    tree-sitter
+  ];
+  programs.direnv.enable = true;
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name  = "Richard Slindee";
+        email = "rslindee@ford.com";
+      };
+    };
+    includes = [
+      { path = "~/dotfiles/git/.config/git/config"; }
+    ];
+  };
+
+  programs.diff-highlight = {
+    enable = true;
+    enableGitIntegration = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      luafile ~/dotfiles/nvim/.config/nvim/init.lua
+    '';
+  };
+
+  programs.zsh = {
+    enable = true;
+    initContent = ''
+      source ~/dotfiles/zsh/.zshrc
+    '';
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+  };
+
+  programs.home-manager.enable = true;
+}
+
