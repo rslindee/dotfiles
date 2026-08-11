7013dc9350378dc7bca6f9bfdf40c49b3167b072
Richard Slindee <rslindee@ford.com>
2026-08-11 10:39:05 -0700

    nvim: Removes custom diagnostic severity sort

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -268,7 +268,7 @@ vim.diagnostic.config({
 	signs = true,
 	underline = true,
 	update_in_insert = false,
-	severity_sort = false,
+	severity_sort = true,
 })
 
 -- Setup language servers.
@@ -1052,41 +1052,6 @@ vim.keymap.set(
 	":CopilotChatToggle<cr>",
 	{ noremap = true, silent = true, desc = "CopilotChat - Toggle CopilotChat pane" }
 )
-
---show a sign for the highest severity diagnostic on a given line:
--- Create a custom namespace. This will aggregate signs from all other
--- namespaces and only show the one with the highest severity on a
--- given line
-local ns = vim.api.nvim_create_namespace("my_namespace")
-
--- Get a reference to the original signs handler
-local orig_signs_handler = vim.diagnostic.handlers.signs
-
--- Override the built-in signs handler
-vim.diagnostic.handlers.signs = {
-	show = function(_, bufnr, _, opts)
-		-- Get all diagnostics from the whole buffer rather than just the
-		-- diagnostics passed to the handler
-		local diagnostics = vim.diagnostic.get(bufnr)
-
-		-- Find the "worst" diagnostic per line
-		local max_severity_per_line = {}
-		for _, d in pairs(diagnostics) do
-			local m = max_severity_per_line[d.lnum]
-			if not m or d.severity < m.severity then
-				max_severity_per_line[d.lnum] = d
-			end
-		end
-
-		-- Pass the filtered diagnostics (with our custom namespace) to
-		-- the original handler
-		local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
-		orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
-	end,
-	hide = function(_, bufnr)
-		orig_signs_handler.hide(ns, bufnr)
-	end,
-}
 
 -- Generate and display code coverage
 vim.keymap.set("n", "<leader>tg", ":Coverage<CR>")
